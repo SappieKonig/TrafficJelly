59ccbe18901e4a7e383e7d2ba14f6f68609118f3
ignace konig <ignacekonig@ignaces-Air.fritz.box>
2023-11-04 12:50:25 +0100

    readded chrono

--- cpp/src/traffic_model.cpp
+++ cpp/src/traffic_model.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <chrono>
 
 TrafficModel::TrafficModel(std::string fn, float delta_time)
     : delta_time(delta_time), population(0), global_time(0)
